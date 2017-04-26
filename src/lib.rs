1b8e9ef671be1a058c1d7de6681ed3521eca9b8e
Eric Andres <emandres@gmail.com>
2017-04-26 13:40:54 -0600

    Copy README from other project

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
-}
+#![allow(dead_code)]
+
+
+//! Rust Tutorial
+//!
+//! Uncomment the lines containing `pub mod chapter_x;` to move forward in the tutorial
+
+extern crate serde_json;
+
+//pub mod chapter_1;
+//pub mod chapter_2;
+pub mod chapter_3;
